2dda6263440a1853a254629e5bb666004df714a0
Avyaan Verma <114806843+avyaanverma@users.noreply.github.com>
2023-07-18 20:49:56 +0530

    Drum kit Event Listener

---
+++ index.js
@@ -0,0 +1,14 @@
+debugger
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++ ){
+    debugger
+    document.querySelectorAll("button")[i].addEventListener("click", actionClick)
+}
+
+function actionClick(){
+    
+    alert("I got clicked! ");
+    console.log("hi ")
+}
+
+
+
